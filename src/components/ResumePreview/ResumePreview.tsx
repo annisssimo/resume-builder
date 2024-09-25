8b5835c120bbc38917f7010de83f05bc39b6b444
Anna <anyalar04@gmail.com>
2024-09-26 00:13:35 +0300

    feat: implement pdf exporting

--- src/components/ResumePreview/ResumePreview.tsx
+++ src/components/ResumePreview/ResumePreview.tsx
@@ -1,3 +1,4 @@
+import React, { useRef } from 'react';
 import { PersonalInfo } from '../../types/personalInfo';
 import './ResumePreview.css';
 import { MdEmail, MdPerson, MdWork } from 'react-icons/md';
@@ -8,6 +9,8 @@ import { Job } from '../../types/job';
 import { Education } from '../../types/education';
 import { FaCubesStacked } from 'react-icons/fa6';
 import { Skills } from '../../types/skills';
+import html2pdf from 'html2pdf.js';
+import { IoMdCloudDownload } from 'react-icons/io';
 
 interface ResumePreviewProps {
   personalInfo: PersonalInfo;
@@ -24,108 +27,133 @@ const ResumePreview: React.FC<ResumePreviewProps> = ({
   jobs,
   education,
 }) => {
+  const resumeRef = useRef<HTMLDivElement>(null);
+
+  const downloadResume = () => {
+    if (resumeRef.current) {
+      const element = resumeRef.current;
+      const opt = {
+        margin: 0,
+        filename: `${personalInfo.name}_${personalInfo.lastName}_Resume.pdf`,
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      };
+
+      html2pdf().from(element).set(opt).save();
+    }
+  };
+
   return (
-    <div className="resume-preview">
-      <div className="resume-left-side">
-        <div className="photo-container">
-          {personalInfo.photo ? (
-            <img
-              src={
-                typeof personalInfo.photo === 'string'
-                  ? personalInfo.photo
-                  : URL.createObjectURL(personalInfo.photo)
-              }
-              alt="person"
-              className="profile-photo"
-            />
-          ) : (
-            'No photo upload'
-          )}
-        </div>
-
-        <div className="left-section">
-          <h3 className="left-side-heading">
-            <MdPerson />
-            About Me
-          </h3>
-          <div className="left-side-text"> {personalInfo.about}</div>
-        </div>
-
-        <div className="left-section">
-          <h3 className="left-side-heading">
-            <FaCubesStacked />
-            Skills
-          </h3>
-          <div className="left-side-text"> {skills.skills}</div>
-        </div>
-
-        <div className="left-section">
-          <h3 className="left-side-heading">
-            <BiSolidContact />
-            Contact
-          </h3>
-          <div className="left-side-text">
-            <FaPhone /> {contactInfo.phone}
+    <div>
+      <IoMdCloudDownload onClick={downloadResume} className="download-fixed" />
+      <div className="resume-preview" ref={resumeRef}>
+        {/* Ваш контент резюме */}
+        <div className="resume-left-side">
+          <div className="photo-container">
+            {personalInfo.photo ? (
+              <img
+                src={
+                  typeof personalInfo.photo === 'string'
+                    ? personalInfo.photo
+                    : URL.createObjectURL(personalInfo.photo)
+                }
+                alt="person"
+                className="profile-photo"
+              />
+            ) : (
+              'No photo upload'
+            )}
           </div>
-          <div className="left-side-text">
-            <MdEmail /> {contactInfo.email}
+
+          <div className="left-section">
+            <h3 className="left-side-heading">
+              <MdPerson />
+              About Me
+            </h3>
+            <div className="left-side-text"> {personalInfo.about}</div>
           </div>
-          <div className="left-side-text">
-            <FaLinkedinIn />
-            {contactInfo.linkedin}
+
+          <div className="left-section">
+            <h3 className="left-side-heading">
+              <FaCubesStacked />
+              Skills
+            </h3>
+            <div className="left-side-text"> {skills.skills}</div>
+          </div>
+
+          <div className="left-section">
+            <h3 className="left-side-heading">
+              <BiSolidContact />
+              Contact
+            </h3>
+            <div className="left-side-text">
+              <FaPhone /> {contactInfo.phone}
+            </div>
+            <div className="left-side-text">
+              <MdEmail /> {contactInfo.email}
+            </div>
+            <div className="left-side-text">
+              <FaLinkedinIn />
+              {contactInfo.linkedin}
+            </div>
+          </div>
+        </div>
+        <div className="resume-right-side">
+          <div className="heading">
+            <p className="user-name">{personalInfo.name}</p>
+            <p className="last-name">{personalInfo.lastName}</p>
+            <p className="job">{personalInfo.jobPosition}</p>
+          </div>
+
+          <div className="work-experience">
+            <h3 className="left-side-heading">
+              <MdWork />
+              Work experience
+            </h3>
+            {jobs.map((j) => (
+              <div key={j.id}>
+                <hr />
+                <p className="company-position">
+                  {j.position} at {j.company}
+                </p>
+                <p className="dates">
+                  <i>
+                    {j.startDate} - {j.endDate}
+                  </i>
+                </p>
+                <p className="description">{j.description}</p>
+              </div>
+            ))}
+          </div>
+
+          <div className="edu-section">
+            <h3 className="left-side-heading">
+              <FaUniversity />
+              Education
+            </h3>
+
+            {education.map((e) => (
+              <div key={e.id}>
+                <hr />
+                <p className="company-position">
+                  {e.degree} at {e.institution}
+                </p>
+                <p className="dates">
+                  <i>
+                    {e.startDate} - {e.endDate}
+                  </i>
+                </p>
+                <p className="description">{e.description}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="resume-right-side">
-        <div className="heading">
-          <p className="user-name">{personalInfo.name}</p>
-          <p className="last-name">{personalInfo.lastName}</p>
-          <p className="job">{personalInfo.jobPosition}</p>
-        </div>
-
-        <div className="work-experience">
-          <h3 className="left-side-heading">
-            <MdWork />
-            Work experience
-          </h3>
-          {jobs.map((j) => (
-            <div key={j.id}>
-              <hr />
-              <p className="company-position">
-                {j.position} at {j.company}
-              </p>
-              <p className="dates">
-                <i>
-                  {j.startDate} - {j.endDate}
-                </i>
-              </p>
-              <p className="description">{j.description}</p>
-            </div>
-          ))}
-        </div>
-
-        <div className="edu-section">
-          <h3 className="left-side-heading">
-            <FaUniversity />
-            Education
-          </h3>
-
-          {education.map((e) => (
-            <div key={e.id}>
-              <hr />
-              <p className="company-position">
-                {e.degree} at {e.institution}
-              </p>
-              <p className="dates">
-                <i>
-                  {e.startDate} - {e.endDate}
-                </i>
-              </p>
-              <p className="description">{e.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <button onClick={downloadResume} className="download-btn">
+        <IoMdCloudDownload />
+        Download
+      </button>
     </div>
   );
 };
